0c9ddf9ed31b4af5dbb7ffce6998778788bca6ce
Rodrigo Souza <rsouza01@gmail.com>
2017-12-02 18:46:11 -0200

    Install and stuff

---
+++ src/iris.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+from sklearn.datasets import load_iris
+
+
+iris_dataset = load_iris()
---
+++ src/pkg_version.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import sys
+import pandas as pd
+import matplotlib
+import numpy as np
+import scipy as sp
+import IPython
+import sklearn
+
+print("Python version: {}".format(sys.version))
+print("pandas version: {}".format(pd.__version__))
+print("matplotlib version: {}".format(matplotlib.__version__))
+print("NumPy version: {}".format(np.__version__))
+print("SciPy version: {}".format(sp.__version__))
+print("IPython version: {}".format(IPython.__version__))
+print("scikit-learn version: {}".format(sklearn.__version__))
