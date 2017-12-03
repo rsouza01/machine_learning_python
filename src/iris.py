ebfdd16bba564a968363b14f6b3919ec6d3c4b14
Rodrigo Souza <rsouza01@gmail.com>
2017-12-03 00:26:02 -0200

    First lessons...

--- src/iris.py
+++ src/iris.py
@@ -1,6 +1,70 @@
 #!/usr/bin/env python
 
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy as sp
+
 from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+
+import sys
+
+sys.path.insert(0, './mglearn')
+
+import mglearn
+from sklearn.neighbors import KNeighborsClassifier
+
 
 
 iris_dataset = load_iris()
+
+print("Keys of iris_dataset: \n{}".format(iris_dataset.keys()))
+print(iris_dataset['DESCR'][:300] + "\n...")
+print("Target names: {}".format(iris_dataset['target_names']))
+print("Feature names: \n{}".format(iris_dataset['feature_names']))
+print("Type of data: {}".format(type(iris_dataset['data'])))
+print("Shape of data: {}".format(iris_dataset['data'].shape))
+print("First five columns of data:\n{}".format(iris_dataset['data'][:5]))
+print("Type of target: {}".format(type(iris_dataset['target'])))
+print("Shape of target: {}".format(iris_dataset['target'].shape))
+print("Target:\n{}".format(iris_dataset['target']))
+
+
+# Training and testing data...
+X_train, X_test, y_train, y_test = train_test_split(iris_dataset['data'], iris_dataset['target'], random_state=0)
+
+print("X_train shape: {}".format(X_train.shape))
+print("y_train shape: {}".format(y_train.shape))
+
+print("X_test shape: {}".format(X_test.shape))
+print("y_test shape: {}".format(y_test.shape))
+
+
+# create dataframe from data in X_train
+# label the columns using the strings in iris_dataset.feature_names
+iris_dataframe = pd.DataFrame(X_train, columns=iris_dataset.feature_names)
+
+# create a scatter matrix from the dataframe, color by y_train
+grr = pd.plotting.scatter_matrix(iris_dataframe, c=y_train, figsize=(15, 15), marker='o', hist_kwds={'bins': 20}, s=60, alpha=.8, cmap=mglearn.cm3)
+
+fig, ax = plt.subplots()
+
+iris_dataframe.plot(ax=ax)
+
+#plt.show() # plt in place of ax
+
+knn = KNeighborsClassifier(n_neighbors=1)
+knn.fit(X_train, y_train)
+
+
+KNeighborsClassifier(algorithm='auto', leaf_size=30, metric='minkowski', metric_params=None, n_jobs=1, n_neighbors=1, p=2, weights='uniform')
+
+
+X_new = np.array([[5, 2.9, 1, 0.2]])
+print("X_new.shape: {}".format(X_new.shape))
+
+
+prediction = knn.predict(X_new)
+print("Prediction: {}".format(prediction))
+print("Predicted target name: {}".format(iris_dataset['target_names'][prediction]))
